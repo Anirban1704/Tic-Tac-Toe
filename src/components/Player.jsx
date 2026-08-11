4f3c9c7902f24854ecff45bb00e3d5dcc7a070fc
iconic <122547494+Anirban1704@users.noreply.github.com>
2026-08-11 17:45:24 +0000

    Couple of changes regarding state 1. Make Edit button functional. 2. Learn changing bool value in state using function and prevvalue.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import {useState} from "react";
 
 export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+  
+  function handleEditClick(){
+    setIsEditing(isEditing => !isEditing);
+  }
+  function handleChange(event){
+    setPlayerName(event.target.value);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+      {!isEditing ? <span className="player-name">{playerName}</span> : <input type="text" required value={playerName} onChange={handleChange}/>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save" }</button>
     </li>
   );
 }
